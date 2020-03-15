6c5a1d960b9413d1144d3613e3ea982a6df8c41c
Vilmantas Gegzna <GegznaV@gmail.com>
2020-03-15 17:01:27 +0200

    Prevent from duplicated file names.

--- R/insert_citation.R
+++ R/insert_citation.R
@@ -795,7 +795,7 @@ get_bib_from_yaml <- function(yaml_delimiters, file_contents, rmd_path, bib_file
       file.path(rmd_path, yaml::yaml.load(yaml_front_matter)$bibliography)
       , tools::file_path_as_absolute
     )
-    c(bib_file, absolute_bib_file)
+    unique(c(bib_file, absolute_bib_file))
   } else {
     NULL
   }
