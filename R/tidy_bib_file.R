5c4348c4915817ada72f5fbbf6fcb29a1bdc5d53
Ilse Pit <ilsepit@gmail.com>
2021-01-05 13:23:28 +0000

    Update tidy_bib_file.R
    changed wording in line 6 from 'list' to 'vector' to properly reflect the fact that the input for messy_bibliography should be in the form of either "messy_references.bib" or c("messy1.bib", "messy2.bib")

--- R/tidy_bib_file.R
+++ R/tidy_bib_file.R
@@ -3,7 +3,7 @@
 #' Removes duplicate and unneeded entries from a Bib(La)Tex-file.
 #'
 #' @param rmd_file Character. One or more paths to the R Markdown files that use the messy bibliography file.
-#' @param messy_bibliography Character. One path or a list of paths to the messy bibliography file(s).
+#' @param messy_bibliography Character. One path or a vector of paths to the messy bibliography file(s).
 #' @param file Character. Path and name for the to-be-created tidy bibliography. If \code{NULL} the messy bibliography is replaced.
 #' @inheritParams query_bib
 #'
